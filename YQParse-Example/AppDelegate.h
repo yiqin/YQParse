85d36f359bd050a1f9cf4cf9f2568a8cc217b879
yiqin <yiqin@uchicago.edu>
2014-09-30 22:09:46 -0500

    init

---
+++ YQParse-Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YQParse-Example
+//
+//  Created by yiqin on 9/30/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YQParse-Example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YQParse-Example
+//
+//  Created by yiqin on 9/30/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
